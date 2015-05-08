e7f460ce4f5552a2b7f6c8e04139f58ff2b3cfc8
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2015-05-08 12:01:27 -0400

    commit

--- server/dal/index.js
+++ server/dal/index.js
@@ -2,7 +2,13 @@ var mongoose = require("mongoose");
 
 var db = mongoose.connect(AppConfigs.dbConStr);
 
-var feedSourceCRUD = require('./feedSource/feedSourceCRUD');
+var feedSourceCRUD = require('./feedSource/feedSourceCRUD', function(err) {
+    if(err) {
+        console.log('Connection Error: Mongoose cannot connect to MongoDB ', err);
+    } else {
+        console.log('Connection Successful: Mongoose successfully connected to MongoDB ');
+    }
+});
 
 module.exports = {
     db: db,
